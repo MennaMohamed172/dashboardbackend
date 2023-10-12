1c3b55deaf3f8243be347724d45c7cc2c00de1c4
MennaMohamed <88841916+mennaohamed@users.noreply.github.com>
2023-10-12 16:10:51 +0300

    Update register.js

--- routers/register.js
+++ routers/register.js
@@ -1,37 +1,13 @@
-const express = require("express")
+const express = require('express')
+const userController = require('../controllers/registerController')
+
 const router = express.Router()
-const registerController = require("../controllers/registerController")
-const auth = require('../middleware/auth')
-const checkRole = require("../middleware/role")
-const { Admin } = require("mongodb")
-const {Receptionist}= require("mongodb")
-// const { USER_ROLE, ASSOCIATION_ROLE } = require('../constants/roles')
-router.post("/register", async (req, res) => {
-  await registerController.addNewUser(req, res)
-})
 
-router.get("/register", auth,(req, res) => {
-  registerController.getAllUsers(req, res)
-})
 
-router.get("/register/:userId", auth,checkRole(Admin,Receptionist),(req, res) => {
-  registerController.getUserById(req, res)
-})
-
-router.put("/register/:id", auth,checkRole(Admin,Receptionist),async (req, res) => {
-  await registerController.updateUserInfoById(req, res)
-})
-
-router.delete("/register/:userId", auth,checkRole(Admin,Receptionist),(req, res) => {
-  registerController.deleteUserById(req, res)
-})
-
-router.post("/login", async (req, res) => {
-  await registerController.adduserRegisteralready(req, res)
-})
-
-router.patch("/logout/:_id",auth,checkRole(Admin,Receptionist), async (req, res) => {
-  await registerController.deleteUser(req, res)
-})
+router.post('/user', userController.addNewUser)
+router.get('/user', userController.getAllUsers)
+router.get('/user/:id', userController.getUserById)
+router.put('/user/:id', userController.updateUserInfoById)
+router.delete('/user/:id', userController.deleteUserById)
 
 module.exports = router
