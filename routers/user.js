a0b9c868042c27a1ee245ab6e9d212b8c7e9333b
MennaMohamed <88841916+mennaohamed@users.noreply.github.com>
2023-10-12 15:47:52 +0300

    Update and rename register.js to user.js

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userController = require('../controllers/registerController')
+const userController = require('../controllers/userControllers')
 
 const router = express.Router()
 
